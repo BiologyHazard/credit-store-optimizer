cc5483e9396a6236ecd136cf4328208f47852156
BioHazard <3482991796@qq.com>
2025-07-11 11:40:42 +0800

    Refactor code structure for improved readability and maintainability

--- 信用交易所商品信息.py
+++ 信用交易所商品信息.py
@@ -10,19 +10,21 @@ from pathlib import Path
     '家具零件': 0.0001,
     '加急许可': 0.001,
 }
+信用交易所物品名称列表: list[str] = []
 信用交易所商品名称列表: list[str] = []
 信用交易所商品价格字典: dict[str, int] = {}
 信用交易所商品理智价值字典: dict[str, float] = {}
 with open(信用交易所商品信息路径, 'r', encoding='utf-8', newline='') as csvfile:
     csv_reader = csv.DictReader(csvfile, delimiter='\t', quotechar='|')
     for row in csv_reader:
-        名称 = row['名称']
-        物品 = row['物品']
+        商品名称 = row['名称']
+        物品名称 = row['物品']
         数量 = int(row['数量'])
         if not row['价格']:  # 跳过 讯使、嘉维尔、坚雷
             continue
         价格 = int(row['价格'])
-        信用交易所商品名称列表.append(名称)
-        信用交易所商品价格字典[名称] = 价格
-        物品的理智价值 = 物品名称到价值的映射字典[物品]
-        信用交易所商品理智价值字典[名称] = 物品的理智价值 * 数量
+        信用交易所物品名称列表.append(物品名称)
+        信用交易所商品名称列表.append(商品名称)
+        信用交易所商品价格字典[商品名称] = 价格
+        物品的理智价值 = 物品名称到价值的映射字典[物品名称]
+        信用交易所商品理智价值字典[商品名称] = 物品的理智价值 * 数量
